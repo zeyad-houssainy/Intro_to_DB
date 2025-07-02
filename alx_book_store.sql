595f6dca2430381f17c613d541313a4eaf860176
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-07-02 21:30:32 +0300

    0. A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
 	author_id INT PRIMARY KEY NOT NULL, 
     author_name VARCHAR(215)
 );
 
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
 	book_id INT NOT NULL PRIMARY KEY, 
 	title VARCHAR(30) NOT NULL, 
 	author_id INT  NOT NULL , 
@@ -18,14 +18,14 @@ CREATE TABLE Books (
 
 
 
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
 	customer_id INT PRIMARY KEY NOT NULL,
 	customer_name VARCHAR(215),
 	email VARCHAR(215),
 	address TEXT
 );
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
 	order_id INT PRIMARY KEY NOT NULL,
 	customer_id INT NOT NULL,
 	order_date DATE NOT NULL,
@@ -34,14 +34,14 @@ CREATE TABLE Orders (
         ON UPDATE CASCADE
 );
 
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
 	orderdetailid INT PRIMARY KEY NOT NULL,
 	order_id INT NOT NULL, 
 	book_id INT NOT NULL, 
     quantity DOUBLE,
-	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+	FOREIGN KEY (order_id) REFERENCES Orders(order_id)
 		ON DELETE CASCADE
-        ON UPDATE CASCADE
+        ON UPDATE CASCADE,
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 		ON DELETE CASCADE
         ON UPDATE CASCADE
